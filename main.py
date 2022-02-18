56db871cfbac66e113edba008b347f1a4c80a4cc
Tera <66988502+TERAKOSHITAISUKE@users.noreply.github.com>
2022-02-18 11:06:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import qrcode
 import numpy as np
 st.title('QRコード自動生成アプリ')
 
-url = st.text_input('QRコードを生成したいURL:')
+# url = st.text_input('QRコードを生成したいURL:')
 
-if st.button('QRコードを生成'):
-    _img = qrcode.make(url)
-    _img.save('qrcode.png')
-    img = Image.open('qrcode.png')
-    st.image(img)
-    st.header("QRコード生成完了")
-    st.balloons()
+# if st.button('QRコードを生成'):
+#     _img = qrcode.make(url)
+#     _img.save('qrcode.png')
+#     img = Image.open('qrcode.png')
+#     st.image(img)
+#     st.header("QRコード生成完了")
+#     st.balloons()
