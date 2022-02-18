252e9a813eb5308606b8312d403f78d6960cfcbe
Tera <66988502+TERAKOSHITAISUKE@users.noreply.github.com>
2022-02-18 11:08:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from PIL import Image
+
 import streamlit as st
-# import qrcode
-import numpy as np
+
 st.title('QRコード自動生成アプリ')
 
-# url = st.text_input('QRコードを生成したいURL:')
-
-# if st.button('QRコードを生成'):
-#     _img = qrcode.make(url)
-#     _img.save('qrcode.png')
-#     img = Image.open('qrcode.png')
-#     st.image(img)
-#     st.header("QRコード生成完了")
-#     st.balloons()
