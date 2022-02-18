a0a0a24f4ac942234cbcf33455a5a5e3926eacd2
TERAKOSHITAISUKE <denpsyadvance@gmail.com>
2022-02-18 01:21:35 +0000

    1st

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import Image
+from PIL import Image
+import streamlit as st
+import qrcode
+import numpy as np
+st.title('QRコード自動生成アプリ')
+
+url = st.text_input('QRコードを生成したいURL:')
+
+if st.button('QRコードを生成'):
+    _img = qrcode.make(url)
+    _img.save('qrcode.png')
+    img = Image.open('qrcode.png')
+    st.image(img)
+    st.header("QRコード生成完了")
+    st.balloons()
